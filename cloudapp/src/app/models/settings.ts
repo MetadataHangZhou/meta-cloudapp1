e83a71cd3809e11d5bfddea5516ef0c6fce8d2ed
addressxy@vip.qq.com <addressxy@vip.qq.com>
2021-03-22 08:56:46 +0800

    update about 'Settings'

---
+++ cloudapp/src/app/models/settings.ts
@@ -0,0 +1,10 @@
+export class Settings {
+    institution:String = '211030';
+    institutionType:String = 'a';
+    holding:String = '905';
+    lookupUrl:String = '/proxy/cgi-bin/fetch_z311.cgi?uname=exlibris&upass=china&key=KEY';
+    lookupPrefix:String ='';
+    classificationNumber:String = 'd';
+    titleNumber:String = 'e';
+    callNo:String = 's';
+}
